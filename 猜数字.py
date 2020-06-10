9372f5fc9d3215240b80f1413e5c3cb9849c853c
a303990788 <52148510+a303990788@users.noreply.github.com>
2020-06-10 15:50:58 +0800

    Create 猜数字.py
    第一次使用GITHUB

---
+++ 猜数字.py
@@ -0,0 +1,18 @@
+import os
+os.chdir('C:\\Users\\Administrator\\AppData\\Local\\Programs\\Python\\Python38')
+import easygui as g
+import random
+y = random.randint(1,1000)
+x = g.enterbox(msg='请输入要猜的数字', title='猜数字小游戏', default='', strip=True, image=None, root=None)
+def gussy(x):
+    if x == y:
+        g.msgbox('恭喜你猜对了')
+    elif x < y:
+        x = g.enterbox(msg='小了！请重新输入要猜的数字', title='猜数字小游戏', default='', strip=True, image=None, root=None)
+        gussy(x)
+    elif x > y:
+        x = g.enterbox(msg='大了！请重新输入要猜的数字', title='猜数字小游戏', default='', strip=True, image=None, root=None)
+        gussy(x)
+        
+        
+        
